cd87da427f137cf244377f7643e72a1a298b2aa1
Bernardo1Tome <132178422+Bernardo1Tome@users.noreply.github.com>
2023-04-30 02:56:44 +0100

    Add files via upload
    Random Useless looped Number giver

---
+++ main (2).cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    
+     string repeat = "no"; 
+     int n;
+     do{
+         //asiuhiad aiduhauidasd8723823b423784 asd8778ad8*(&*&*d *D&*AS&dASd 8&d8AS&d S8d7A*sd)
+        cout << "Please give me a number: ";
+        cin >> n;
+        cout << "do you want to proceed? (yes/no)"<<endl;
+        cin >> repeat;
+     }
+     while(repeat == "yes");
+   
+    
+}
