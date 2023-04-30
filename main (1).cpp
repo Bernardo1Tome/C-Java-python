93f04960694ca237b15211913785caac69704270
Bernardo1Tome <132178422+Bernardo1Tome@users.noreply.github.com>
2023-04-30 02:54:15 +0100

    Add files via upload
    A code just for adding number

---
+++ main (1).cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int a;
+  int b;
+  cout << "Please give me number: ";
+  cin >> a;
+  cout << "PLease give me another number";
+  cin >> b;
+  cout << "The sum of " << a << " and " << b << " is:" << a+b;
+  
+  
+
+
+
+  
+}
+
+
